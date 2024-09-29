10ba927b13416e3d1936b2fe2e60932c766f75e7
MaulanaBintang <117064989+MaulanaBintang@users.noreply.github.com>
2024-09-29 19:19:20 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -34,7 +34,7 @@ def total_casual_data(day_data):
    return cas_data
 
 def sum_order (hour_data):
-    sum_order_items_data = hour_data.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
+    sum_order_items_data = hour_data.groupby("one_of_week").count_cr.sum().sort_values(ascending=False).reset_index()
     return sum_order_items_data
 
 def macem_season (day_data): 
@@ -117,43 +117,26 @@ ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
 st.pyplot(fig)
 
-st.subheader("pada jam berapa yang paling banyak dan paling sedikit disewa?")
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+st.subheader("pada hari apa yang paling banyak dan paling sedikit disewa?")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(50, 20))
 
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_data.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
+sns.barplot(x="one_of_week", y="count_cr", data=sum_order_items_data.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
 ax[0].set_ylabel(None)
-ax[0].set_xlabel("Hours (PM)", fontsize=30)
-ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
+ax[0].set_xlabel("Day", fontsize=30)
+ax[0].set_title("Hari dengan banyak penyewa sepeda", loc="center", fontsize=30)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
  
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_data.sort_values(by="hours", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
+sns.barplot(x="one_of_week", y="count_cr", data=sum_order_items_data.sort_values(by="one_of_week", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
 ax[1].set_ylabel(None)
-ax[1].set_xlabel("Hours (AM)",  fontsize=30)
-ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
+ax[1].set_xlabel("Day",  fontsize=30)
+ax[1].set_title("Hari dengan sedikit penyewa sepeda", loc="center", fontsize=30)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
 ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
  
-st.pyplot(fig)
-st.subheader("musim apa yang paling banyak disewa?")
-
-colors = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#90CAF9"]
-fig, ax = plt.subplots(figsize=(20, 10))
-sns.barplot(
-        y="count_cr", 
-        x="season",
-        data=season_data.sort_values(by="season", ascending=False),
-        palette=colors,
-        ax=ax
-    )
-ax.set_title("Grafik Antar Musim", loc="center", fontsize=50)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='x', labelsize=35)
-ax.tick_params(axis='y', labelsize=30)
 st.pyplot(fig)
 
 st.subheader("Perbandingan Customer yang Registered dengan casual")
